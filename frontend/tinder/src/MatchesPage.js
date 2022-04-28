1396078d89350abf337f93abf01eb2310eb2ff27
Vuk Perisic <v_perisic@berkeley.edu>
2022-04-28 12:21:48 -0700

    fixed a bug in matches page

--- frontend/tinder/src/MatchesPage.js
+++ frontend/tinder/src/MatchesPage.js
@@ -7,13 +7,22 @@ import axios from 'axios';
 
 const MatchesPage = props => {
     const [matchData, setMatchData] = useState({});
+    let match_objects = [];
     for (var i = 0; i < props.matches.length; i++) {
         axios.get("mongodb://127.0.0.1:27017/tinder2.0/users/" + props.matches[i])
         .then(res => setMatchData(res));
-    return (
-        <Match username = {matchData.username} skills = {matchData.skills} skills_to_learn = {matchData.skills_to_learn} email = {matchData.email} />
-    )
+        match_objects.push(matchData);
     }
+    return (
+        <div>
+            {match_objects.length != 0 ? match_objects.map(el => {
+                return (
+                    <Match username = {el.username} skills = {el.skills} skills_to_learn = {el.skills_to_learn} email = {el.email} />
+                )
+            }) : []};
+        </div>
+    )
 };
 
 export default MatchesPage;
+
