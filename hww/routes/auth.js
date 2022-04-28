1e1cfa93e3e91a6281399b176abaf46b6993fb00
astr0-nat <npentschev@berkeley.edu>
2022-04-28 10:23:48 -0700

    login funtionality

--- hww/routes/auth.js
+++ hww/routes/auth.js
@@ -106,7 +106,58 @@ router.post(
                         
 
 
-    router.post('/login',  )
+    router.post('/login',   async (req, res) => {
+        const errors = validationResult(req);
+    
+        if (!errors.isEmpty()) {
+          return res.status(400).json({
+            errors: errors.array(),
+          });
+        }
+    
+        const { username, password } = req.body;
+        try {
+          let user = await User.findOne({
+            username,
+          });
+          if (!user)
+            return res.status(400).json({
+              message: 'User Not Exist',
+            });
+    
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch)
+            return res.status(400).json({
+              message: 'Incorrect Password !',
+            });
+    
+          const payload = {
+            user: {
+              id: user.id,
+            },
+          };
+    
+          jwt.sign(
+            payload,
+            'randomString',
+            {
+              expiresIn: 3600,
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.status(200).json({
+                token,
+              });
+            }
+          );
+        } catch (e) {
+          console.error(e);
+          res.status(500).json({
+            message: 'Server Error',
+          });
+        }
+      }
+      )
 
     //     router.post('/login',
     //     (req, res) => {
