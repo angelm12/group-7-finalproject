7d93c99a96a4ca463f6d4a2dfd5553b025e76999
Vuk Perisic <v_perisic@berkeley.edu>
2022-04-28 12:34:20 -0700

    added routes to index.js for HomePage and MatchesPage

--- frontend/tinder/src/index.js
+++ frontend/tinder/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Match from './Match.js';
+import MatchesPage from './MatchesPage.js';
+import HomePage from './HomePage.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +14,19 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route path = "/MatchesPage">
+        <MatchesPage />
+      </Route>
+      <Route path = "/HomePage">
+        <HomePage />
+      </Route>
+    </Switch>
+  </Router>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
