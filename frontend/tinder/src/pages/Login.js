4001e5af1f303cf67ef68279d4dbeeb7693d8f70
MattM944 <mojica.matthew02@gmail.com>
2022-04-28 15:44:29 -0700

    Changes to login page

--- frontend/tinder/src/pages/Login.js
+++ frontend/tinder/src/pages/Login.js
@@ -7,7 +7,6 @@ import "../css/Login.css"
 
 import axios from 'axios'
 const url = "http://localhost:3030/auth/" 
-// "http://upandcoming-env.eba-icsyb2cg.us-east-1.elasticbeanstalk.com/auth/"
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -20,16 +19,16 @@ class SignIn extends React.Component {
     loginAttempt = (e)  => {
         e.preventDefault();
         console.log("login attempt")
-        const user = this.usernameRef.current.value;
+        const uName = this.usernameRef.current.value;
         const pass = this.passwordRef.current.value;
         axios.post(url +"login", {
-            username: user,
+            username: uName,
             password: pass
         }).then((result) => {
                 if (result.data.message === "success") {
                     console.log("Logged in! Token: " + result.data.token);
                     localStorage.setItem('token', result.data.token)
-                    localStorage.setItem('user', JSON.stringify(result.data.user))
+                    localStorage.setItem('username', JSON.stringify(result.data.email))
                     this.props.history.push('/browse');
                 } else {
                     console.log("User not logged in properly");
@@ -58,34 +57,32 @@ class SignIn extends React.Component {
         const { incorrect } = this.state;
         let warn = <div></div>;
         if (incorrect) {
-            warn = <div className="warning">Incorrect username/password. Try again.</div>
+            warn = <div className="warning">Incorrect email/password. Try again.</div>
 
         }
         return(
-            <div className = 'wrapper-signin' style={{display: "block"}}>
-                <div className = 'nav-bar' >
-                        <Header />
-                </div>
-                <div className = "separator-signin">
-
-                </div>
+            <div className = 'wrapper-signin' style={{display: "inline"}}>
                 <div className='content'>
-                    <div className="title">Sign In</div>
+                    <div className="title">Kindling</div>
+                    <div className="welcome">Welcome to Kindling, please login.</div>
                     {warn}
-                <Form className="form">
-                    <Form.Group className = "field" controlId="formBasicUsername">
-                        <Form.Control ref={this.usernameRef} type="name" placeholder="Username" className="secondry-font"/>
-                    </Form.Group>
+                    <Form className="form">
+                        <Form.Group className = "field" controlId="formBasicUsername">
+                            <Form.Control ref={this.usernameRef} type="name" placeholder="Username" className="secondry-font"/>
+                        </Form.Group>
 
-                    <Form.Group className = "field" controlId="formBasicPassword">
-                        <Form.Control ref={this.passwordRef} type="password" placeholder="Password" className="secondry-font"/>
-                    </Form.Group>
+                        <Form.Group className = "field" controlId="formBasicPassword">
+                            <Form.Control ref={this.passwordRef} type="password" placeholder="Password" className="secondry-font"/>
+                        </Form.Group>
 
-                    <Button className = "submit-button" id="login" onClick={(e) => this.loginAttempt(e)} variant="primary" type="submit">
-                        Login
-                    </Button>
-                </Form>
-                <div className="subtext" onClick={(e) => this.switch(e)}>Don't have an account?</div>
+                        <Button className = "submit-button" id="login" onClick={(e) => this.loginAttempt(e)} variant="primary" type="submit">
+                            Login
+                        </Button>
+                    </Form>
+                    <div className="subtext" onClick={(e) => this.switch(e)}>Don't have an account?</div>
+                </div>
+                <div>
+                    <img src={Kindling} className="kindling" alt = ""/>
                 </div>
             </div>
         )
