49e5b26d4d5d152f8de055073f6a1c3de848680a
Angel Mau <angelmau@berkeley.edu>
2022-04-28 18:11:29 -0700

    pushing rendered signup page

--- frontend/tinder/src/components/Signup.js
+++ frontend/tinder/src/components/Signup.js
@@ -1,4 +1,4 @@
-import './Signup.css';
+import './Createuser.css';
 import { useState } from 'react';
 // import { createUserWithEmailAndPassword, 
 //     onAuthStateChanged
@@ -9,10 +9,10 @@ import axios from 'axios'
 const url = "http://localhost:3030/auth/" 
 
 
-// function Createuser() {
+function Createuser() {
 
-//     // const [registerEmail, setRegisterEmail] = useState("");
-//     // const [registerPw, setRegisterPw] = useState("");
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPw, setRegisterPw] = useState("");
 //     // let navigate = useNavigate();
 
 //     // const [user, setUser] = useState({});
@@ -32,75 +32,13 @@ const url = "http://localhost:3030/auth/"
 
 
 
-class signup extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {signuped: false, incorrect: false}
-        this.usernameRef = React.createRef();
-        this.passwordRef = React.createRef();
-        this.realNameRef = React.createRef();
-        this.emailRef = React.createRef();
-    }
 
-
-    switch = (e) => {
-        console.log()
-        this.setState({switchPage: true})
-        e.preventDefault();
-        console.log('switch')
-        this.props.history.push('/signin');
-    }
-
-    signupAttempt = (e)  => {
-        e.preventDefault();
-        console.log("signup attempt")
-        const uName = this.usernameRef.current.value;
-        const pwd = this.passwordRef.current.value;
-        const realName = this.realNameRef.current.value;
-        const email = this.emailRef.current.value;
-
-
-        axios.post(url + "signup", {
-            username: uName,
-            password: pwd,
-            realname: realName,
-            email: email,
-        }).then((result) => {
-                if (result.data.message === "success") {
-                    let id = result.data.id
-                    console.log("signuped success in! Token: ");
-                    this.switch(e)
-                } else {
-                    console.log("Did not signup");
-                    this.setState({incorrect: true})
-                     // switch page to proper url
-                }},(err) => {
-                    console.log("Did not signup");
-                    this.setState({incorrect: true})
-                    console.log(err)
-                }
-            )
-    }
-  
-    checkUploadResult = (resultEvent) => {
-        if (resultEvent.event === 'success') {
-            console.log(resultEvent.info.secure_url)
-            this.setState({noMoreUpload: true, profileUrl: resultEvent.info.secure_url})
-        }
-    }
-    render() {
-        const { incorrect } = this.state;
-        let warn = <div></div>;
-        if (incorrect) {
-            warn = <div className="warning">Failed to signup. Please try again with a new username.</div>
-        }
     return (
 
     <body className = "CreateuserBackground">
         <div className="centerInfoDiv">
             <div className="contents">
-
-                <div  className="logo">
+                <div className="logo">
                     Kindling
                 </div>
                 <div className = "createAccount">Create account</div>
@@ -108,7 +46,7 @@ class signup extends React.Component {
                     <div className="emailField">
                         <div className="usernameTitle">
                             Email Address
-                            {/* <input className="emailInput" type="text" placeholder="Email" onChange={(e) => setRegisterEmail(e.target.value)}/> */}
+                            <input className="emailInput" type="text" placeholder="Email" onChange={(e) => setRegisterEmail(e.target.value)}/>
                         </div>
 
                         <div className = "userLogo">
@@ -120,11 +58,11 @@ class signup extends React.Component {
                     <div className="passwordField">
                         <div className="passwordTitle">
                             Password
-                            {/* <input className="passwordInput" type="password" placeholder="Password" onChange={(e) => setRegisterPw(e.target.value)}/> */}
+                            <input className="passwordInput" type="password" placeholder="Password" onChange={(e) => setRegisterPw(e.target.value)}/>
                         </div>
                             <div className="underlineText2">
                             </div>
-                            <button onClick={register}>Submit</button>
+                            <button>Submit</button>
                     </div>
                 </div>
             </div>
@@ -139,4 +77,4 @@ class signup extends React.Component {
 };
 
 
-export default withRouter(Signup);
+export default Createuser;
