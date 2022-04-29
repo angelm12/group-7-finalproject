58188bb08954c4376ee42472b2420b20c2e4e3ad
Vuk Perisic <v_perisic@berkeley.edu>
2022-04-28 17:30:40 -0700

    removed hard coding

--- frontend/tinder/src/MatchesPage.js
+++ frontend/tinder/src/MatchesPage.js
@@ -6,46 +6,14 @@ import axios from 'axios';
 //props contains the User's information, but we only really need their matches' usernames for this component 
 
 const MatchesPage = props => {
-    //const [matchData, setMatchData] = useState({});
-    //let match_objects = [];
-    //for (var i = 0; i < props.matches.length; i++) {
-        //axios.get("mongodb://127.0.0.1:27017/tinder2.0/users/" + props.matches[i])
-        //.then(res => setMatchData(res));
-        //match_objects.push(matchData);
-    //}
-    const match_objects = [
-        {
-            username: "Angelthegod",
-            skills: ["React", "kickboxing", "chess grandmaster"],
-            skills_to_learn: ["Ninjitsu", "gourmet cooking"],
-            email: "angelj5@gmail.com"
-        }, 
-        {
-            username: "NatPent",
-            skills: ["Node", "kickboxing", "chess grandmaster"],
-            skills_to_learn: ["Ninjitsu", "gourmet cooking"],
-            email: "natp5@gmail.com"
-        },
-        {
-            username: "VukP",
-            skills: ["React", "kickboxing", "chess grandmaster"],
-            skills_to_learn: ["Ninjitsu", "gourmet cooking"],
-            email: "vukp5@gmail.com"
+    const [matchData, setMatchData] = useState({});
+    let match_objects = [];
+    for (var i = 0; i < props.matches.length; i++) {
+        axios.get("mongodb://127.0.0.1:27017/tinder2.0/users/" + props.matches[i])
+        .then(res => setMatchData(res));
+        match_objects.push(matchData);
+    }
 
-        }, 
-        {
-            username: "AdiP", 
-            skills: ["React", "kickboxing", "chess grandmaster"],
-            skills_to_learn: ["Ninjitsu", "gourmet cooking"],
-            email: "adip5@gmail.com"
-        }, 
-        {
-            username: "MatM", 
-            skills: ["React", "kickboxing", "chess grandmaster"],
-            skills_to_learn: ["Ninjitsu", "gourmet cooking"],
-            email: "matm5@gmail.com"
-        }
-    ];
     return (
         <div>
             {match_objects.length != 0 ? match_objects.map(el => {
